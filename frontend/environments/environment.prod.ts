a2dddb505c2c4069e2d06d6f554bc8a233e27ab6
FranckTano <tanoguyfranck22@gmail.com>
2026-06-03 17:39:56 +0000

    fix: urls prod Render

--- frontend/environments/environment.prod.ts
+++ frontend/environments/environment.prod.ts
@@ -1,6 +1,5 @@
-// Remplacez par l'URL exacte de votre service Render (frontOffice backend)
 export const environment = {
     production: true,
-    apiUrl: 'https://VOTRE_FRONTOFFICE_BACKEND.onrender.com/api',
-    url: 'https://VOTRE_FRONTOFFICE.vercel.app'
+    apiUrl: 'https://shop-droping-front-backend.onrender.com/api',
+    url: 'https://shop-droping-frontoffice.vercel.app'
 };
